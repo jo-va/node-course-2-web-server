51adcfad54760580bcea67921a6a0ef3944a8630
jo-va <vandamme_jonathan@hotmail.com>
2017-12-30 20:52:40 -0500

    Added projects page

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Portfolio Page'
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
